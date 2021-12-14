c91b1349e69251b579483af8da5ed54625ec5271
PiekharievaK <pechareva@gmail.com>
2021-12-14 16:08:58 +0200

    Добавляет стартовые файлы и минимальные настройки

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './sass/main.scss';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import axios from "axios";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const BASE_URL = `https://pixabay.com/api/`;
+const PixabeyKey = `24814635 - 98ab646e956d73723bbfbc5eb`;
